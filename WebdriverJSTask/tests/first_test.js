70aff73ba2e0bfa97d6f7b18aa520359b01bfb9f
Kiryl Bahdanets <kiryl_bahdanets@epam.com>
2017-10-27 15:00:51 +0300

    test

--- WebdriverJSTask/tests/first_test.js
+++ WebdriverJSTask/tests/first_test.js
@@ -16,23 +16,26 @@ driver.manage().window().maximize();
 
 const world = require('../page_objects/world');
 
-driver.get(world.homePage.homePageUrl).then(() =>{
-    return driver.findElement(world.homePage.header.loginButton).click();
-}).then(() => {
-    return driver.findElement(world.loginPage.emailField).sendKeys("bogdanetskirill@gmail.com");
-}).then(() => {
-    return driver.findElement(world.loginPage.passwordField).sendKeys("qweasdzxc123");
-}).then(() => {
-    driver.findElement(world.loginPage.enteringButton).click();
-}).then(() =>{
-    return driver.wait(driver.findElement(world.homePage.header.habrahabrLogo), 5000);
-}).then(() => {
-    driver.sleep(1000);
-    return driver.findElement(world.homePage.header.habrahabrLogo).click();
-}).then(() => {
-    return driver.findElement(world.homePage.header.loggedUsersButton).click();
-}).then(() => {
-    return driver.findElement(world.homePage.header.loggedUsersProfile).getText();
-}).then((text) => {
-    console.log(text);
-});
+module.exports = (() => {
+    driver.get(world.homePage.homePageUrl).then(() =>{
+        return driver.findElement(world.homePage.header.loginButton).click();
+    }).then(() => {
+        return driver.findElement(world.loginPage.emailField).sendKeys("bogdanetskirill@gmail.com");
+    }).then(() => {
+        return driver.findElement(world.loginPage.passwordField).sendKeys("qweasdzxc123");
+    }).then(() => {
+        driver.findElement(world.loginPage.enteringButton).click();
+    }).then(() =>{
+        return driver.wait(driver.findElement(world.homePage.header.habrahabrLogo), 5000);
+    }).then(() => {
+        driver.sleep(1000);
+        return driver.findElement(world.homePage.header.habrahabrLogo).click();
+    }).then(() => {
+        return driver.findElement(world.homePage.header.loggedUsersButton).click();
+    }).then(() => {
+        return driver.findElement(world.homePage.header.loggedUsersProfile).getText();
+    }).then((text) => {
+        console.log(text);
+        return text;
+    });
+})();
